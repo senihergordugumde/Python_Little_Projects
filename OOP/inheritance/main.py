344795401163ef481d4104247ad254ab4376f8e7
Emir <mail@emiraksu.net>
2023-02-18 16:24:17 +0300

    Override / super fonksiyonu ders notum.

--- OOP/inheritance/main.py
+++ OOP/inheritance/main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+class Calısan(): #Class Oluşturduk
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, isim, maas, departman):
+        print("Çalışan sınıfının init fonksiyonu")
+
+        self.isim = isim
+        self.maas = maas
+        self.departman = departman
+
+    def bilgilerigoster(self): # Bu method çalışan sınıfının bilgilerini yazdıracak.
+
+        print("Çalışan sınıfının bilgileri....")
+
+        print(" İsim : {}\n Maaş : {}\n Departman : {}\n" .format(self.isim, self.maas, self.departman))
+
+    def departman_degistir(self, yeni_departman):
+        self.departman = yeni_departman
+
+class Yonetici(Calısan):  #Yönetici classı Calısan Classının özelliklerini miras alıyor
+
+    def __init__(self,isim, maas, departman, kisi_sayisi): #Fazladan bir özellik daha ekledik. Override etmiş olduk
+        super().__init__(isim,maas,departman) # Tekrar aynı bilgileri yazmamak için super kullanıyoruz
+
+        self.kisi_sayisi = kisi_sayisi
+    def zam_yap(self, zam_miktari):
+        self.maas += zam_miktari
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+yonetici = Yonetici("Emir",5000,"IT",10)
 
+yonetici.zam_yap(31)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+yonetici.bilgilerigoster()
