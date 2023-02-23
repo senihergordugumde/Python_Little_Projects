294c6f38a762a2bb686ce146205f2ca5db53fd56
Emir <mail@emiraksu.net>
2023-02-23 22:05:10 +0300

    Kütüphane Güncellemesi

--- [05] sql/main.py
+++ [05] sql/main.py
@@ -1,26 +1,28 @@
 import sqlite3
+con = sqlite3.connect("kutuphane.db")
+cursor = con.cursor()
+class Kitaplik():
+    def __init__(self,kitap_adi, yazar, yayinevi, sayfa):
 
-con = sqlite3.connect("kutuphane.db") #varsa bağlanacak yoksa oluşturacak
+        self.kitap_adi = kitap_adi
+        self.yazar = yazar
+        self.yayinevi = yayinevi
+        self.sayfa = sayfa
 
-cursor = con.cursor() #imleçi cursora atıyoruz.
+    def tablo_olustur(self):
+        cursor.execute("CREATE TABLE IF NOT EXISTS kitaplik (Kitap TEXT, Yazar TEXT, Yayinevi TEXT, Sayfa INT)")
+        con.commit()
+    def kitap_ekle(self):
+        cursor.execute("INSERT INTO kitaplik VALUES (?, ?, ?, ?)" ,(self.kitap_adi, self.yazar, self.yayinevi, self.sayfa))
+        con.commit()
 
-def tablo_olustur():
-    cursor.execute("CREATE TABLE IF NOT EXISTS kitaplik (Isim TEXT, Yazar TEXT, Yayinevi TEXT, Sayfa INT)") #Tablo oluşturduk
-    con.commit()
 
-def veri_ekle():
-    cursor.execute("INSERT INTO kitaplik VALUES ('Hükümdar', 'Makyavel', 'İş Bankası', 331)")
-    con.commit()
-
-def veri_ekle2(isim, yazar, yayin_evi, sayfa_sayisi):
-    cursor.execute("INSERT INTO kitaplik VALUES (?, ?, ?, ?)",(isim,yazar,yayin_evi,sayfa_sayisi ))
-    con.commit()
-
-isim = input("Kitap adı: ")
+kitap_adi = input("Kitap adı: ")
 yazar = input("Yazar: ")
-yayin_evi = input("Yayınevi: ")
-sayfa_sayisi = int(input("Sayfa sayısı: "))
-tablo_olustur()
-veri_ekle()
-veri_ekle2(isim,yazar,yayin_evi,sayfa_sayisi)
-con.close() #bağlantıyı kapatıyoruz
+yayinevi = input("Yayınevi: ")
+sayfa = int(input("Sayfa sayısı: "))
+
+kitaplik = Kitaplik(kitap_adi,yazar,yayinevi,sayfa)
+
+kitaplik.tablo_olustur()
+kitaplik.kitap_ekle()
